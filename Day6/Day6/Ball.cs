c17fd97304f4ecfa72a861f54061ce875ddbca1a
Hawaiikiki <45608699+Hawaiikiki@users.noreply.github.com>
2022-05-25 23:34:50 -0700

    Add files via upload

---
+++ Day6/Day6/Ball.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public class Ball
+    {
+        private int size;
+        private Color color;
+        private int numThrown;
+        public int Size { get; set; }
+        public Color Color { get { return color; } set { color = value; } }
+
+        public int NumThrown { get { return numThrown; } set { numThrown = value; } }
+
+        public Ball(int size, Color color)
+        {
+            Size = size;
+            if (size <= 0)
+            {
+                throw new ArgumentException("ball size should be greater than 0");
+            }
+            Color = color;
+            NumThrown = 0;
+        }
+
+        public void Pop()
+        {
+            Size = 0;
+        }
+
+        public void Throw()
+        {
+            if (Size != 0)
+            {
+                NumThrown++;
+            }
+            else if (Size == 0)
+            {
+                Console.WriteLine("Cannot throw popped ball");
+            }
+            
+        }
+
+        public int CountThrow()
+        {
+            return numThrown;
+        }
+
+    }
+}
---
+++ Day6/Day6/Color.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public class Color
+    {
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; }
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+
+        }
+        public Color(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = 255;
+
+        }
+        public byte GreyScale()
+        {
+
+            return (byte)((Red+Green+Blue)/3);
+        }
+    }
+}
